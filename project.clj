f8949abdfe2c307f2d802b86f8b5522624eefad7
Gary Fredericks <fredericksgary@gmail.com>
2014-05-13 10:04:46 -0500

    Import for tests from test.check patch
    Am about to make a test for a bug I found.

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies
+                   [[org.clojure/test.check "0.5.7"]]}}
 
   :plugins [[lein-release "1.0.5"]]
   :lein-release {:deploy-via :clojars})
---
+++ test/com/gfredericks/test/chuck/generators_test.clj
@@ -0,0 +1,31 @@
+(ns com.gfredericks.test.chuck.generators-test
+  (:require [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [com.gfredericks.test.chuck.generators :as gen']))
+
+(def lists-and-counts
+  (gen'/for [nums (gen/vector gen/nat)
+             :let [cardinality (count nums)]]
+    [nums cardinality]))
+
+(defspec for-works-correctly 100
+  (prop/for-all [[nums cardinality] lists-and-counts]
+    (= (count nums) cardinality)))
+
+(def lists-with-two-of-their-elements
+  (gen'/for [nums (gen/vector gen/nat)
+             :let [cardinality (count nums)]
+             :when (> cardinality 1)
+             x (gen/elements nums)
+             :let [[befores [_x & afters]] (split-with #(not= % x) nums)
+                   nums-x (concat befores afters)]
+             y (gen/elements nums-x)]
+    [nums x y]))
+
+(defspec complex-for-works-correctly 100
+  (prop/for-all [[nums x y] lists-with-two-of-their-elements]
+    (let [f (frequencies nums)]
+      ;; check that both x and y are in the list
+      (or (and (= x y) (> (f x) 1))
+          (and (not= x y) (pos? (f x)) (pos? (f y)))))))
