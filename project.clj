8aa65af675bd8b844da83e36c079ef5eb42e4005
Gary Fredericks <fredericksgary@gmail.com>
2014-12-06 23:01:42 -0600

    Upgrade the heck out of test.check

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev {:dependencies
-                   [[org.clojure/test.check "0.5.7"]
+                   [[org.clojure/test.check "0.6.1"]
                     [instaparse "1.3.4"]]}})
