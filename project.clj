9a751f3577453361ffa27cf5bb6a313439f701f2
Gary Fredericks <fredericksgary@gmail.com>
2014-12-23 13:41:16 -0500

    Whoopsiedoodle: instaparse is a runtime dep
    Fixes issue #2.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/fredericksgary/test.chuck"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [instaparse "1.3.5"]]
   :deploy-repositories [["releases" :clojars]]
   :profiles {:dev {:dependencies
-                   [[org.clojure/test.check "0.6.2"]
-                    [instaparse "1.3.5"]]}})
+                   [[org.clojure/test.check "0.6.2"]]}})
