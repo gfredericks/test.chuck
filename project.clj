2635fca85430a6ad5451e608a98c43eed9d476d8
Gary Fredericks <fredericksgary@gmail.com>
2014-12-22 21:50:57 -0500

    Set clojars as deploy repo

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
+  :deploy-repositories [["releases" :clojars]]
   :profiles {:dev {:dependencies
                    [[org.clojure/test.check "0.6.2"]
                     [instaparse "1.3.5"]]}})
