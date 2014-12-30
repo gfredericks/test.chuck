dc4fa0da5d4f776bb78395bbf144cf212087eb0e
Gary Fredericks <fredericksgary@gmail.com>
2014-12-30 12:39:47 -0600

    Create an alias to run tests on different JVMs
    Uses implicit profiles, I'm not sure if there's a better way to do
    that.

--- project.clj
+++ project.clj
@@ -7,4 +7,12 @@
                  [instaparse "1.3.5"]]
   :deploy-repositories [["releases" :clojars]]
   :profiles {:dev {:dependencies
-                   [[org.clojure/test.check "0.6.2"]]}})
+                   [[org.clojure/test.check "0.6.2"]]}}
+  :aliases {"test-all"
+            ^{:doc "Runs tests on multiple JVMs; profiles java-7
+                    and java-8 should be defined outside this project."}
+            ["do"
+             "clean,"
+             "with-profile" "+java-7" "test,"
+             "clean,"
+             "with-profile" "+java-8" "test"]})
