2264c0124d594ab75c66dd7578950c4f855df170
Gary Fredericks <fredericksgary@gmail.com>
2015-01-31 15:08:42 -0600

    Add a regression test for exception-catching in .clojure-test

--- test/com/gfredericks/test/chuck/clojure_test_test.clj
+++ test/com/gfredericks/test/chuck/clojure_test_test.clj
@@ -16,3 +16,21 @@
       (is (= @c (inc i)))
       (swap! c inc)
       (is (> @c 0)))))
+
+(deftest exception-detection-test
+  (eval '(do (ns fake.test.namespace
+               (:require [clojure.test :refer :all]
+                         [clojure.test.check.generators :as gen]
+                         [com.gfredericks.test.chuck.clojure-test :refer :all]))
+             (deftest this-test-should-crash
+               (checking "you can divide four by numbers" 100 [i gen/pos-int]
+                 ;; going for uncaught-error-not-in-assertion here
+                 (let [n (/ 4 i)]
+                   (is n))))))
+  (let [test-results
+        (binding [clojure.test/*test-out* (java.io.StringWriter.)]
+          (clojure.test/run-tests (the-ns 'fake.test.namespace)))]
+    ;; should this be reported as an error for sure?
+    (is (= 1 (+ (:error test-results)
+                (:fail test-results)))))
+  (remove-ns 'fake.test.namespace))
