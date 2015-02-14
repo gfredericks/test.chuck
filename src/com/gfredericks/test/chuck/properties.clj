e745dd0fcbcb7cf5cacbdddd83c8c1ee191f8751
Gary Fredericks <fredericksgary@gmail.com>
2015-02-14 10:08:24 -0600

    Better arg handling for properties/for-all
    This specifically addresses the fact that in the old code failing
    cases were reported as opaque functions, which is unhelpful.
    
    Basically rewrote the whole thing by having the macro figure out
    what names are bound exactly so that it can just generate a map of
    symbols to pass into the property body.

--- src/com/gfredericks/test/chuck/properties.clj
+++ src/com/gfredericks/test/chuck/properties.clj
@@ -4,15 +4,48 @@
             [clojure.test.check.properties :as prop]
             [com.gfredericks.test.chuck.generators :as gen']))
 
+(declare for-bindings)
+
+;;
+;; At some point it would be cleaner to separate bindings used
+;; directly with generators from bindings used with :let, and hide the
+;; latter (in metadata?) so it doesn't come up as an arg to the
+;; property
+;;
+
+(defn ^:private for-bindings-in-binding-expr
+  [expr]
+  ;; this'll get gensyms o_O
+  (->> (clojure.core/destructure [expr :dummy])
+       (partition 2)
+       (map first)))
+
+(defn ^:private for-bindings-in-clause
+  [left right]
+  (cond (= :let left) (->> right
+                           (partition 2)
+                           (map first)
+                           (mapcat for-bindings-in-binding-expr))
+        (= :when left) []
+        (= :parallel left) (for-bindings right)
+        (or (symbol? left) (map? left) (vector? left)) (for-bindings-in-binding-expr left)
+        :else (throw (ex-info "Unrecognized binding expression in test.chuck.properties/for-all!"
+                              {:expr left}))))
+
+(defn ^:private for-bindings
+  [clauses]
+  (->> (partition 2 clauses)
+       (mapcat (fn [[left right]] (for-bindings-in-clause left right)))
+       (distinct)))
+
 (defmacro for-all
   "Alternative version of clojure.test.check.properties/for-all where
   the binding forms are interpreted as per
   com.gfredericks.test.chuck.generators/for."
   [bindings expr]
-  `(gen/fmap
-
-    ;; calling a private function because I don't know of a cleaner
-    ;; way to do this :/
-    (comp (#'prop/apply-gen #(%)) list)
-
-    (gen'/for ~bindings (fn [] ~expr))))
+  (let [bound-names (for-bindings bindings)
+        quoted-names (map #(list 'quote %) bound-names)]
+    `(prop/for-all [{:syms [~@bound-names]}
+                    (gen'/for ~bindings
+                      ~(zipmap quoted-names bound-names))]
+       ~expr)))
--- test/com/gfredericks/test/chuck/properties_test.clj
+++ test/com/gfredericks/test/chuck/properties_test.clj
@@ -10,3 +10,12 @@
               (:result
                (t.c/quick-check 100 (prop'/for-all [x gen/int]
                                       (/ 4 0)))))))
+
+(deftest reported-args-test
+  (let [p (prop'/for-all [x gen/nat]
+            (not (<= 0 x 10)))
+        {:keys [fail]} (t.c/quick-check 1000 p)]
+    (is (= 1 (count fail)))
+    (let [[m] fail]
+      (is (= ['x] (keys m)))
+      (is (<= 0 (get m 'x) 10)))))
