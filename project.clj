0d555e28bfc2aa8ef3f1d969daad45585ba1be66
Gary Fredericks <fredericksgary@gmail.com>
2015-06-22 10:09:49 -0500

    Version 0.1.20-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/test.chuck "0.1.19"
+(defproject com.gfredericks/test.chuck "0.1.20-SNAPSHOT"
   :description "A dumping ground of test.check utilities"
   :url "https://github.com/fredericksgary/test.chuck"
   :license {:name "Eclipse Public License"
