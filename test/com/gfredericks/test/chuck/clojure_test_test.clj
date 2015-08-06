242f8bb17aaaaa1a428e4b264c06b7d90a4f2abe
Nicolas Berger <nicoberger@gmail.com>
2015-08-06 17:03:05 -0300

    Move exception-handling-test to a separate ns
    Isolates this test because it needs whitelisting of the tests to
    run (to exclude `this-test-should-crash`), which can be error-prone.

--- test/com/gfredericks/test/chuck/clojure_test_test.clj
+++ test/com/gfredericks/test/chuck/clojure_test_test.clj
@@ -18,26 +18,6 @@
       (swap! c inc)
       (is (> @c 0)))))
 
-(deftest this-test-should-crash
-  (checking "you can divide four by numbers" 100 [i gen/pos-int]
-    ;; going for uncaught-error-not-in-assertion here
-    (let [n (/ 4 i)]
-      (is n))))
-
-(deftest exception-detection-test
-  (let [test-results
-        (binding [; need to keep the failure of this-is-supposed-to-fail from
-                  ; affecting the clojure.test.check test run
-                  *report-counters* (ref *initial-report-counters*)
-                  *test-out* (java.io.StringWriter.)]
-          (test-var #'this-test-should-crash)
-          @*report-counters*)]
-    ;; should be reported as an error, but it's being reported as :fail :/
-    (is (= {:pass 0
-            :fail 1
-            :error 0}
-           (select-keys test-results [:pass :fail :error])))))
-
 (deftest for-all-test
   (let [passing-prop (for-all [x gen/s-pos-int]
                        (is (< x (+ x x))))]
@@ -49,9 +29,3 @@
                        (is (zero? x))
                        (is (= x x)))]
     (is (not (:result (quick-check 20 failing-prop))))))
-
-(defn test-ns-hook []
-  (test-vars [#'for-all-test
-              #'counter
-              #'integer-facts
-              #'exception-detection-test]))
