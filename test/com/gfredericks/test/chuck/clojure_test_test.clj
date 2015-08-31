5cbeb207793900f60e59cffc677d6dcaa5802d9b
Colin Williams <lackita@gmail.com>
2015-08-31 08:19:24 -0400

    renamed test in correct place

--- test/com/gfredericks/test/chuck/clojure_test_test.clj
+++ test/com/gfredericks/test/chuck/clojure_test_test.clj
@@ -18,24 +18,6 @@
       (swap! c inc)
       (is (> @c 0)))))
 
-(deftest exception-detection-test
-  (eval '(do (ns fake.test.namespace
-               (:require [clojure.test :refer :all]
-                         [clojure.test.check.generators :as gen]
-                         [com.gfredericks.test.chuck.clojure-test :refer :all]))
-             (deftest this-test-should-crash-and-be-caught
-               (checking "you can divide four by numbers" 100 [i gen/pos-int]
-                 ;; going for uncaught-error-not-in-assertion here
-                 (let [n (/ 4 i)]
-                   (is n))))))
-  (let [test-results
-        (binding [clojure.test/*test-out* (java.io.StringWriter.)]
-          (clojure.test/run-tests (the-ns 'fake.test.namespace)))]
-    ;; should this be reported as an error for sure?
-    (is (= 1 (+ (:error test-results)
-                (:fail test-results)))))
-  (remove-ns 'fake.test.namespace))
-
 (deftest for-all-test
   (let [passing-prop (for-all [x gen/s-pos-int]
                        (is (< x (+ x x))))]
